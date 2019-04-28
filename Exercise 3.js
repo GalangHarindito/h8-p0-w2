5f278e606eb6ff70b61fecb0be74a05bc2cdbcad
GalangHarindito <49673012+GalangHarindito@users.noreply.github.com>
2019-04-28 16:52:32 +0700

    Add files via upload

---
+++ Exercise 3.js
@@ -0,0 +1,28 @@
+var peran = 'Penyihir';
+var nama = 'Zero';
+
+if ( nama == '' && peran == ''){
+    console.log('Nama harus diisi!');
+} else {
+    console.log('');
+}
+if ( nama == 'Mikael' && peran == ''){
+    console.log ('Halo '+ nama + ', Pilih peranmu untuk memulai game!');
+} else {
+    console.log ();
+}
+if ( nama == 'Nina' && peran == 'Ksatria'){
+    console.log ('Selamat datang di Dunia Proxitya, ' + nama + '\nHalo' +' '+ peran +' '+ nama + ', kamu dapat menyerang dengan senjatamu!');
+} else {
+    console.log ();
+}
+if ( nama == 'Danu' && peran == 'Tabib'){
+    console.log ('Selamat datang di Dunia Proxitya, ' + nama + '\nHalo' +' '+ peran +' '+ nama + ', kamu akan membantu temanmu yang terluka.');
+} else {
+    console.log ();
+}
+if ( nama == 'Zero' && peran == 'Penyihir'){
+    console.log ('Selamat datang di Dunia Proxitya, ' + nama + '\nHalo' +' '+ peran +' '+ nama + ', ciptakan keajaiban yang membantu kemenanganmu!');
+} else {
+    console.log ();
+}
---
+++ Exercise 4.js
@@ -0,0 +1,19 @@
+var month = 12
+var date = '1';
+var year = '2008';
+switch (month) {
+    case 1 : month = 'January' ; break;
+    case 2 : month = 'February'; break;
+    case 3 : month = 'Maret'; break;
+    case 4 : month ='April'; break;
+    case 5 : month ='May'; break;
+    case 6 : month ='Juny'; break;
+    case 7 : month ='July'; break;
+    case 8 : month ='Augustus'; break;
+    case 9 : month ='September'; break;
+    case 10 : month ='October'; break;
+    case 11 : month ='November'; break;
+    case 12 : month ='Desember'; break;
+    
+}  console.log( date + ' ' + month + ' ' +year);
+    
